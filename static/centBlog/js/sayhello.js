2fd595b02a3c7c1abcaad7fcd04f21d66b664403
Dandelion <dandelionatcha@163.com>
2020-11-04 23:12:29 +0800

    v1.8.9

--- static/centBlog/js/sayhello.js
+++ static/centBlog/js/sayhello.js
@@ -14,7 +14,10 @@ function sayHello(jquery_elem_id) {
     else if (hour < 14)        {$(jquery_elem_id)[0].innerHTML = "辛苦了，来一杯下午茶吧，放松一下"}
     else if (hour < 17)        {$(jquery_elem_id)[0].innerHTML = "下午好，这将是一个等待延续的故事"}
     else if (hour < 19)        {$(jquery_elem_id)[0].innerHTML = "夕阳好漂亮啊，一起去看看吧"}
-    else if (hour < 23)        {$(jquery_elem_id)[0].innerHTML = "生活总会在意外之间变得美妙，晚安"}
-    else                       {$(jquery_elem_id)[0].innerHTML = "有些条路你必须坚持下去，保持清醒"}
+    else if (hour < 23) {
+        $(jquery_elem_id)[0].innerHTML = "生活总会在意外之间变得美妙，晚安"
+    } else {
+        $(jquery_elem_id)[0].innerHTML = "有些路你必须坚持下去，保持清醒"
+    }
 }
 
